3eb99c3d1a7d9a3a0f34ca45aea818f5991502a1
jbrijs <joebrijs@gmail.com>
2024-07-27 22:31:45 -0600

    added evaluation to training step

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -3,11 +3,6 @@ import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
-X_train_sequences = torch.load('X_train_sequences.pt')
-y_train_sequences = torch.load('y_train_sequences.pt')
-X_test_sequences = torch.load('X_test_sequences.pt')
-y_test_sequences = torch.load('y_test_sequences.pt')
-
 
 class LSTMModel(nn.Module):
     def __init__(self, num_features, hidden_dim, num_layers, output_size=1):
@@ -24,7 +19,7 @@ class LSTMModel(nn.Module):
         out = self.fc(out[:, -1, :]) 
         return out
 
-def train_model(model, train_loader, epochs):
+def train_evaluate_model(model, train_loader, test_loader, epochs):
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     
@@ -34,23 +29,44 @@ def train_model(model, train_loader, epochs):
             optimizer.zero_grad()
             outputs = model(X_batch)
             loss = criterion(outputs.squeeze(), y_batch)
-            loss.backware()
+            loss.backward()
             optimizer.step()
             total_loss += loss.item()
 
         if (epoch+1) % 100 == 0:
             print(f'Epoch {epoch+1}, Average Loss: {total_loss / len(train_loader)}')
 
-def main():
+         # Evaluation phase
+    model.eval()
+    total_test_loss = 0
+    with torch.no_grad():
+        for X_batch, y_batch in test_loader:
+            outputs = model(X_batch)
+            loss = criterion(outputs.squeeze(), y_batch)
+            total_test_loss += loss.item()
+
+    average_test_loss = total_test_loss / len(test_loader)
+    print(f'Test Loss: {average_test_loss}')
+
+def main(ticker):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    num_features = X_train_sequences.shape[2]
 
-    model = LSTMModel(num_features, hidden_dim=50, num_layers=2).to(device)
-
+    X_train_sequences = torch.load(f'../data/{ticker}_sequences/X_train_sequences.pt').to(device)
+    y_train_sequences = torch.load(f'../data/{ticker}_sequences/y_train_sequences.pt').to(device)
+    X_test_sequences = torch.load(f'../data/{ticker}_sequences/X_test_sequences.pt').to(device)
+    y_test_sequences = torch.load(f'../data/{ticker}_sequences/y_test_sequences.pt').to(device)
+    
     train_data = TensorDataset(X_train_sequences.to(device), y_test_sequences.to(device))
     train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+    test_data = TensorDataset(X_test_sequences, y_test_sequences)
+    test_loader = DataLoader(test_data, batch_size=64, shuffle=False)
 
-    train_model(model, train_loader, epochs=500)
+    num_features = X_train_sequences.shape[2]
+    model = LSTMModel(num_features, hidden_dim=50, num_layers=2, output_size=1)
+    model.to(device)
+
+
+    train_evaluate_model(model, train_loader, test_loader, epochs=500)
 
 if __name__ == '__main__':
     main()
