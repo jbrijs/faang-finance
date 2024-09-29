794f503870f8f749947bf043191c860bc6e125b4
jbrijs <joebrijs@gmail.com>
2024-09-29 17:29:10 -0600

    docker file set up for lambda layer

--- image/src/make_prediction.py
+++ image/src/make_prediction.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from flask import json
 import torch
 import numpy as np
-from lstm_model import LSTMModel
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import joblib
@@ -15,6 +14,21 @@ import os
 ALPHA_VANTAGE_API_KEY = os.environ.get('ALPHA_VANTAGE_API_KEY')
 s3 = boto3.client('s3')
 BUCKET_NAME = 'faangfinance'
+
+class LSTMModel(nn.Module):
+    def __init__(self, num_features, hidden_dim, num_layers, output_size=1):
+        super(LSTMModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(num_features, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).to(x.device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :]) 
+        return out
 
 
 SPLITS = {
