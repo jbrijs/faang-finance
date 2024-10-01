353db56a27e8113d37ad22cfa87c0237a9630d93
jbrijs <joebrijs@gmail.com>
2024-09-30 22:18:00 -0600

    exception handling and json dumps

--- lambda_functions/get_predictions.py
+++ lambda_functions/get_predictions.py
@@ -8,36 +8,47 @@ bucket_name = 'faangfinance'
 
 
 def get_prediction(ticker):
-    file_key = f'predictions/{ticker}_predictions.csv'
-    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-    data = obj['Body'].read()
+    try:
+            
+        file_key = f'predictions/{ticker}_predictions.csv'
+        obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+        data = obj['Body'].read()
 
-    df = pd.read_csv(BytesIO(data))
-    df['time_stamp'] = pd.to_datetime(df['time_stamp'])
+        df = pd.read_csv(BytesIO(data))
+        df['time_stamp'] = pd.to_datetime(df['time_stamp'])
 
-    most_recent_row = df.loc[df['time_stamp'].idxmax()]
-    return most_recent_row['prediction']
+        most_recent_row = df.loc[df['time_stamp'].idxmax()]
+        return most_recent_row['prediction']
+    except Exception as e:
+        print(f"Error fetching prediction for {ticker}: {e}")
+        return None
+
 
 def get_previous_close(ticker):
-    file_key = f'data/{ticker}_daily_data.csv'
-    obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-    data = obj['Body'].read()
+    try:
+        file_key = f'data/{ticker}_daily_data.csv'
+        obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+        data = obj['Body'].read()
 
-    df = pd.read_csv(BytesIO(data))
-    df['time_stamp'] = pd.to_datetime(df['time_stamp'])
+        df = pd.read_csv(BytesIO(data))
+        df['time_stamp'] = pd.to_datetime(df['time_stamp'])
 
-    most_recent_row = df.loc[df['time_stamp'].idxmax()]
-    return most_recent_row['close']
+        most_recent_row = df.loc[df['time_stamp'].idxmax()]
+        return most_recent_row['close']
+    except Exception as e:
+        print(f"Error fetching previous close for {ticker}: {e}")
+        return None
 
 
 def lambda_handler(event, context):
     predictions = {}
     tickers = ['AAPL', 'GOOG', 'META', 'NFLX', 'AMZN', 'NVDA', 'MSFT', 'ADBE']
     for ticker in tickers:
-        predictions[ticker] = {'prediction': get_prediction(ticker), 'prevClose': get_previous_close(ticker)}
-    
-    
-    json_data = df.to_json(orient='records')
+        predictions[ticker] = {
+            'prediction': get_prediction(ticker),
+            'prevClose': get_previous_close(ticker)}
+
+    json_data = json.dumps(predictions)
 
     return {
         'statusCode': 200,
